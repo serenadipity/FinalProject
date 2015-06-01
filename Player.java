edc179f9bd8ffd7b5ac3bd97af2fc6aa9df308ba
serenadipity <serenagchan@gmail.com>
2015-06-01 09:19:43 -0400

    Yasss

---
+++ Player.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Player {
+
+    private String sport;
+    private int age;
+    private boolean gender;
+
+    public Player(String s, int a, boolean g) {
+	sport = s;
+	age = a;
+	gender = g;
+    }
+
+    public String getSport() {
+	return sport;
+    }
+
+    public int getAge() {
+	return age;
+    }
+
+    public boolean getGender() {
+	return gender;
+    }
+
+    
+}
---
+++ Team.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Team {
+
+    private Player[] team;
+    
+    public Team(int p) {
+	team = new Player[p];
+    }
+
+    
+
+}
+
+    
